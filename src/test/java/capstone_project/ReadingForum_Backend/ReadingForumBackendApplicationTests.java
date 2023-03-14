e6b22afa2dc850c634ac073ef0a5302ebeb08918
hathat <mjdfx120312@163.com>
2023-03-14 11:32:16 +0800

    2023-3-14

---
+++ src/test/java/capstone_project/ReadingForum_Backend/ReadingForumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone_project.ReadingForum_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingForumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
